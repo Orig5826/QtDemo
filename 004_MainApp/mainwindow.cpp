eaee6c062943fb5584488a61259bdff07c82226d
jf_t96e <Orig5826@163.com>
2018-09-08 18:49:44 +0800

    通过代码添加菜单

--- 004_MainApp/mainwindow.cpp
+++ 004_MainApp/mainwindow.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "qicon.h"
+#include "qaction.h"
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QAction * openAction = new QAction(tr("打开&(O)"),this);
+    QIcon icon(":/myimages/images/fileopen.png");
+    // 添加图标
+    openAction->setIcon(icon);
+    // 设置快捷键
+    openAction->setShortcut(QKeySequence(tr("Ctrl+O")));
+    // 在文件菜单中设置重新打开的动作
+    ui->menu_F->addAction(openAction);
 }
 
 MainWindow::~MainWindow()
