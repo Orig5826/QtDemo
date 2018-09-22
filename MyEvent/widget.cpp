7643c13e3c0e01dcbcb3ab45d6453314d6b7b533
jf_t96e <Orig5826@163.com>
2018-09-23 01:03:14 +0800

    上传 ：鼠标，键盘，定时器，随机数时间 ....忘了，时间太晚了，随机数没有弄好，下次啊

---
+++ MyEvent/widget.cpp
@@ -0,0 +1,94 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QMouseEvent>
+#include <QKeyEvent>
+
+#include <QTimer>
+#include <QDateTime>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    TimerInit();
+    ui->label->setText("时间和日期");
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::TimerInit()
+{
+    TimerID1 = startTimer(5000);
+    TimerID2 = startTimer(3000);
+    TimerID3 = startTimer(2000);
+
+    // 若仅仅需要少量的定时器，可以通过信号和槽来处理
+    QTimer *timer = new QTimer(this);
+    connect(timer,&QTimer::timeout,this,&Widget::timerUpdate);
+    timer->start(1000);
+}
+
+void Widget::timerUpdate()
+{
+    QDateTime time = QDateTime::currentDateTime();
+    QString str = time.toString("yyyy-MM-dd hh:mm:ss dddd");
+    ui->label->setText(str);
+    ui->label->setAlignment(Qt::AlignCenter);
+}
+
+void Widget::mousePressEvent(QMouseEvent *e)
+{
+    ui->pushButton->setText(tr("坐标:(%1,%2)").arg(e->x()).arg(e->y()));
+}
+
+void Widget::keyPressEvent(QKeyEvent *e)
+{
+    int x = ui->pushButton->x();
+    int y = ui->pushButton->y();
+    switch(e->key())
+    {
+    case Qt::Key_W:
+    {
+        ui->pushButton->move(x,y-10);
+    }break;
+    case Qt::Key_S:
+    {
+        ui->pushButton->move(x,y+10);
+    }break;
+    case Qt::Key_A:
+    {
+        ui->pushButton->move(x-10,y);
+    }break;
+    case Qt::Key_D:
+    {
+        ui->pushButton->move(x+10,y);
+    }break;
+    default:
+        break;
+    }
+}
+
+void Widget::timerEvent(QTimerEvent * event)
+{
+    if(event->timerId() == TimerID1)
+    {
+        ui->lineEdit->setText("Timer_1.1.1");
+    }
+    else if(event->timerId() == TimerID2)
+    {
+        ui->lineEdit->setText("Timer_2.2.2");
+    }
+    else if(event->timerId() == TimerID3)
+    {
+        ui->lineEdit->setText("Timer_3.3.3");
+    }
+    else
+    {
+        qApp->quit();
+    }
+}
---
+++ MyEvent/widget.h
@@ -0,0 +1,46 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+#pragma execution_character_set("utf-8")
+
+
+class QMouseEvent;
+class QKeyEvent;
+class QTimerEvent;
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+
+    int TimerID1,TimerID2,TimerID3;
+    void TimerInit();
+    void timerUpdate();
+
+protected:
+    /* 鼠标和按键 */
+    void mousePressEvent(QMouseEvent *);
+    void keyPressEvent(QKeyEvent *);
+    /*
+     * \breaf 定时器
+     * 定时器事件
+     * 若想开启少量的定时器，也可以使用信号和槽
+     * 见 timerUpdate
+     */
+    void timerEvent(QTimerEvent *);
+    /* 随机数 */
+};
+
+#endif // WIDGET_H
