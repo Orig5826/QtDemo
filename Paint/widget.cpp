76c007e08764abac53a8e6f0baef7164d289315d
jf_t96e <Orig5826@163.com>
2018-11-11 23:42:27 +0800

    编写小程序 涂鸦板(^.^)

--- Paint/widget.cpp
+++ Paint/widget.cpp
@@ -1,14 +1,106 @@
-#include "widget.h"
+﻿#include "widget.h"
 #include "ui_widget.h"
+
+#include "QPixmap"
+#include "QPoint"
+#include "QMouseEvent"
+#include "QPainter"
+
+#include "QFileDialog"
+#include "QMessageBox"
+#include "QString"
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    this->resize(800,600);
+    this->setWindowTitle("涂鸦板");
+
+    pix = QPixmap(600,600);
+    pix.fill(Qt::black);
+
+    pen.setColor(Qt::white);
+    pen.setWidth(5);
+
+    // 在窗口中添加按键清空画布的按键
+    clearButton = new QPushButton(this);
+    clearButton->move(650,500);
+    clearButton->resize(100,60);
+    clearButton->setText(("清空涂鸦板"));
+    connect(clearButton,&QPushButton::clicked,this,&Widget::clear_paint);
+
+    saveButton = new QPushButton(this);
+    saveButton->move(650,400);
+    saveButton->resize(100,60);
+    saveButton->setText(("保存涂鸦板"));
+    connect(saveButton,&QPushButton::clicked,this,&Widget::save_paint);
 }
 
 Widget::~Widget()
 {
+    delete clearButton;
     delete ui;
+}
+
+void Widget::paintEvent(QPaintEvent *)
+{
+    // 在指定的画布上根据鼠标前后两点绘制直线
+    QPainter painter_pix(&pix);
+    painter_pix.setPen(pen);
+    if(lastPoint != endPoint)
+    {
+        painter_pix.drawLine(lastPoint,endPoint);
+
+        // 绘制完毕，给前一个坐标赋值为后一个坐标，作为新的起点坐标
+        lastPoint = endPoint;
+    }
+
+    // 在窗口中进行绘制
+    QPainter painter(this);
+    painter.drawPixmap(0,0,pix);
+}
+
+void Widget::mousePressEvent(QMouseEvent * event)
+{
+    if(event->button() == Qt::LeftButton)
+    {
+        lastPoint = event->pos();
+    }
+    endPoint = lastPoint;
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *event)
+{
+    if(event->buttons() & Qt::LeftButton)
+    {
+        endPoint = event->pos();
+        update();
+    }
+}
+
+void Widget::mouseReleaseEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton)
+    {
+        endPoint = event->pos();
+        update();
+    }
+}
+
+void Widget::clear_paint()
+{
+    pix.fill(Qt::black);
+    update();
+}
+
+void Widget::save_paint()
+{
+    QString name = "随手涂鸦.png";
+    // 设置文件过滤器
+    QString filters("Image (*.png *.jpg *.jpeg *.bmp);;"
+                    "All (*.*)");
+    QString filename = QFileDialog::getSaveFileName(this,"保存涂鸦",name,filters);
+    pix.save(filename);
 }
--- Paint/widget.h
+++ Paint/widget.h
@@ -1,7 +1,13 @@
-#ifndef WIDGET_H
+﻿#ifndef WIDGET_H
 #define WIDGET_H
 
+// 当在windows上使用Qt的时候，添加如下代码
+// 则Qt保存为utf-8文件的时候，生成的程序没有乱码
+#pragma execution_character_set("utf-8")
+
 #include <QWidget>
+#include <QPen>
+#include <QPushButton>
 
 namespace Ui {
 class Widget;
@@ -10,7 +16,22 @@ class Widget;
 class Widget : public QWidget
 {
     Q_OBJECT
+protected:
+    void paintEvent(QPaintEvent *);
+    void mousePressEvent(QMouseEvent *);
+    void mouseMoveEvent(QMouseEvent *);
+    void mouseReleaseEvent(QMouseEvent *);
+private:
+    QPixmap pix;
+    QPoint lastPoint,endPoint;
+    QPen pen;
 
+    QPushButton * clearButton;
+    QPushButton * saveButton;
+
+    // 按键清空画布
+    void clear_paint();
+    void save_paint();
 public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
